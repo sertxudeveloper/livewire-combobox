d97451b7a617b1af026030ed4ee46b4cdb48df45
Sergio Peris <dev.sertxu@gmail.com>
2023-05-10 09:01:56 +0200

    wip

--- resources/views/livewire/combobox.blade.php
+++ resources/views/livewire/combobox.blade.php
@@ -5,7 +5,8 @@
 
     <div class="relative mt-1">
         <input type="search" class="form-input" name="{{ $name }}" id="{{ $name }}" placeholder="{{ $placeholder }}"
-               @focus="open = true" @click="open = true" wire:model.debounce.800ms="search" wire:clear
+               @focus="open = true" @click="open = true" @focusout="open = false"
+               wire:model.debounce.800ms="search" wire:clear
                role="combobox" aria-controls="options" aria-expanded="false" autocomplete="off">
 
         <ul class="absolute z-21 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
